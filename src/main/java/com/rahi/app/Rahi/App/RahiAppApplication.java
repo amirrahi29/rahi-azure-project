799eeb2c9d8290ad31bf83b7342bc2929b6f0221
Amir Rahi <amirrahi29@gmail.com>
2026-03-28 00:04:52 +0530

    first commit

---
+++ src/main/java/com/rahi/app/Rahi/App/RahiAppApplication.java
@@ -0,0 +1,11 @@
+package com.rahi.app.Rahi.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RahiAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RahiAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rahi/app/Rahi/App/RahiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahi.app.Rahi.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RahiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
